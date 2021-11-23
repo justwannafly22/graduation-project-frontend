74b49f0d8dac91b11b15eee64825153051fb3cd9
Egor Dubrouski <nadevatb@gmail.com>
2021-11-23 20:06:13 +0300

    Added add form

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { ReactiveFormsModule  } from '@angular/forms';
 import { ProductsViewComponent } from './products/products-view/products-view.component';
-import { MatTableModule } from '@angular/material/table';
 import { AddProductComponent } from './products/add-product/add-product.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MaterialModule } from './shared/material.module';
+
 
 @NgModule({
   declarations: [
@@ -23,12 +21,8 @@ import { MatPaginatorModule } from '@angular/material/paginator';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule,
     ReactiveFormsModule,
-    MatToolbarModule,
-    MatTableModule,
-    MatFormFieldModule,
-    MatPaginatorModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- project/src/app/products/add-product/add-product.component.ts
+++ project/src/app/products/add-product/add-product.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { Location } from '@angular/common';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-product',
@@ -10,25 +9,42 @@ import { Location } from '@angular/common';
 
 export class AddProductComponent {
   
-  name: string = '';
-  price: number = 0;
+  formGroup!: FormGroup;
+  titleAlert: string = 'This field is required and must be between 5 and 50';
+  post: any = '';
 
-  addForm = this.formBuilder.group(
-    {
-      name: '',
-      price: ''
+  constructor(private readonly formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    let numberRegEx = /^\d*[1-9]\d*$/;
+
+    this.formGroup = this.formBuilder.group({
+      'name': [null, [Validators.required, Validators.minLength(5), Validators.maxLength(50)]],
+      'price': [null, [Validators.required, Validators.pattern(numberRegEx)]],
+      'weight': [null, [Validators.required, Validators.pattern(numberRegEx)]]
     });
+  }
+
+  get name() {
+    return this.formGroup.get('name') as FormControl
+  }
+
+  getErrorPrice() {
+    return this.formGroup.get('price')?.hasError('required') ? 'Field is required' :
+      this.formGroup.get('price')?.hasError('pattern') ? 'Not a valid price. Price can be only a positive number.' : '';
+  }
+
+  getErrorWeight(){
+    return this.formGroup.get('weight')?.hasError('required') ? 'Field is required' :
+      this.formGroup.get('weight')?.hasError('pattern') ? 'Not a valid weight. Weight can be only a positive number.' : '';
+  }
+
+  onSubmit(post: any) {
+    this.post = post;
+  }
   
-  constructor(
-    private readonly formBuilder: FormBuilder,
-    private readonly location: Location
-  ) { }
-
-  onSubmit(): void {
-    console.log(this.addForm.value);
-  }
-
-  getBack(): void{
-    this.location.back();
-  }
 }
