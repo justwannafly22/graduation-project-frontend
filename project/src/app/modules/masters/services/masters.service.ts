32df8daf1beb0706fd2efc4b13c431aae532989f
Egor Dubrovskiy <egor.dubrovskiy@liveswitch.com>
2022-05-30 00:16:32 +0300

    improve master update process

--- project/src/app/modules/masters/services/masters.service.ts
+++ project/src/app/modules/masters/services/masters.service.ts
@@ -6,11 +6,12 @@ import { environment } from "src/app/environments/environment";
 import { MastersRequestInterface } from "src/app/shared/interfaces/masters/masters-request.interface";
 import { MastersResponseInterface } from "src/app/shared/interfaces/masters/masters-response.interface";
 import { SecondMastersRequestInterface } from "src/app/shared/interfaces/masters/second-request.interface";
+import { PersistanceService } from "src/app/shared/services/persistanse.service";
 
 @UntilDestroy()
 @Injectable()
 export class MastersService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private persistanseService:PersistanceService) {}
 
   public getMaster(id: string): Observable<MastersResponseInterface> {
     const url = `${environment.mastersApiUrl}/${id}`;
@@ -45,6 +46,13 @@ export class MastersService {
     const url = `${environment.mastersApiUrl}/${id}`;
     return this.http
       .put<MastersResponseInterface>(url, data)
-      .pipe(untilDestroyed(this),map((response: MastersResponseInterface) => response));
+      .pipe(untilDestroyed(this),map((response: MastersResponseInterface) => {
+        this.persistanseService.set('age', response.age);
+        this.persistanseService.set('contactNumber', response.contactNumber);
+        this.persistanseService.set('email', response.email);
+        this.persistanseService.set('fullName', response.fullName);
+        this.persistanseService.set('id', response.id);
+        this.persistanseService.set('attendeeId', response.attendeeId);
+      return response}));
   }
 }
